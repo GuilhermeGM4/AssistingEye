e7b6f729cfc8518feb2b61684ad15424dd38f5fe
GuilhermeGM4 <guilherme455@pm.me>
2024-11-28 17:36:05 -0300

    Initial commit

---
+++ app/src/main/java/com/assistingeye/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.assistingeye
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.assistingeye.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val amb: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
