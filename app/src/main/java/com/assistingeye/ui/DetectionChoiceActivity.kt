679ee971cdf0f62dc449f67ddcff239f565f0d43
GuilhermeGM4 <guilherme455@pm.me>
2024-11-28 19:48:57 -0300

    feat: Created detection choice activity
    With this, the respective layout has been created.

---
+++ app/src/main/java/com/assistingeye/ui/DetectionChoiceActivity.kt
@@ -0,0 +1,10 @@
+package com.assistingeye.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import com.assistingeye.databinding.ActivityDetectionChoiceBinding
+
+class DetectionChoiceActivity: AppCompatActivity() {
+    private val adcb: ActivityDetectionChoiceBinding by lazy {
+        ActivityDetectionChoiceBinding.inflate(layoutInflater)
+    }
+}
