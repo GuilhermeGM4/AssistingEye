9a19f9b6332d2ec06563e63fa07163c107e86e46
GuilhermeGM4 <guilherme455@pm.me>
2024-11-28 21:58:20 -0300

    feat: Added onCreate override on detection choice

--- app/src/main/java/com/assistingeye/ui/DetectionChoiceActivity.kt
+++ app/src/main/java/com/assistingeye/ui/DetectionChoiceActivity.kt
@@ -1,10 +1,20 @@
 package com.assistingeye.ui
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.assistingeye.databinding.ActivityDetectionChoiceBinding
 
 class DetectionChoiceActivity: AppCompatActivity() {
     private val adcb: ActivityDetectionChoiceBinding by lazy {
         ActivityDetectionChoiceBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(adcb.root)
+
+        adcb.selectImageBt.setOnClickListener{
+            //TODO abrir seletor de imagem
+        }
     }
 }
