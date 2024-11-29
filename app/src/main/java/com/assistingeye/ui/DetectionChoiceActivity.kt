efab3aac292826059c0f137452eb629f7a13f026
GuilhermeGM4 <guilherme455@pm.me>
2024-11-29 18:21:53 -0300

    feat: Added image chooser

--- app/src/main/java/com/assistingeye/ui/DetectionChoiceActivity.kt
+++ app/src/main/java/com/assistingeye/ui/DetectionChoiceActivity.kt
@@ -1,6 +1,10 @@
 package com.assistingeye.ui
 
+import android.net.Uri
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.PickVisualMediaRequest
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.assistingeye.databinding.ActivityDetectionChoiceBinding
 
@@ -9,12 +13,22 @@ class DetectionChoiceActivity: AppCompatActivity() {
         ActivityDetectionChoiceBinding.inflate(layoutInflater)
     }
 
+    private val selectImage = registerForActivityResult(ActivityResultContracts.PickVisualMedia()){ uri ->
+        if(uri != null){
+            Toast.makeText(this, "Imagem selecionada", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            Toast.makeText(this, "Nenhuma imagem selecionada", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(adcb.root)
 
         adcb.selectImageBt.setOnClickListener{
             //TODO abrir seletor de imagem
+            selectImage.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
         }
     }
 }
