7c8c2f19274236830d716524f912c33d02459c33
GuilhermeGM4 <guilherme455@pm.me>
2025-03-29 20:10:19 -0300

    feat: Added adapter
    The adapter were already created in preparation to create a ListView for all required objects found

---
+++ app/src/main/java/com/assistingeye/ui/adapters/RequiredObjectAdapter.kt
@@ -0,0 +1,44 @@
+package com.assistingeye.ui.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.assistingeye.R
+import com.assistingeye.databinding.TileDetectedObjectBinding
+import com.assistingeye.model.DetectedObjectData
+
+class RequiredObjectAdapter(
+    context: Context,
+    private val requiredObjectList: ArrayList<DetectedObjectData>
+): ArrayAdapter<DetectedObjectData>(context, R.layout.tile_detected_object, requiredObjectList) {
+    private data class ObjectTileHolder(val nameTV: TextView)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        lateinit var tdo: TileDetectedObjectBinding
+
+        val detectedObject = requiredObjectList[position]
+
+        var objectTile = convertView
+        if(objectTile == null){
+            tdo = TileDetectedObjectBinding.inflate(
+                context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater,
+                parent,
+                false
+            )
+        }
+
+        objectTile = tdo.root
+
+        val holder = objectTile.tag as ObjectTileHolder
+        holder.let {
+            with(detectedObject){
+                it.nameTV.text = name
+            }
+        }
+
+        return objectTile
+    }
+}
