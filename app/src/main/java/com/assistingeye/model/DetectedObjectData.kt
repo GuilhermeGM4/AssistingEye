96751d600d494a45da9d8d970ab66622215f230c
GuilhermeGM4 <guilherme455@pm.me>
2025-04-01 15:30:32 -0300

    feat: DetectedObjectData is now parcelable

--- app/src/main/java/com/assistingeye/model/DetectedObjectData.kt
+++ app/src/main/java/com/assistingeye/model/DetectedObjectData.kt
@@ -1,9 +1,12 @@
 package com.assistingeye.model
 
 import android.graphics.RectF
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
 
+@Parcelize
 data class DetectedObjectData(
     val confidence: Float,
     val boundingBox: RectF,
     val name: String
-)
+): Parcelable
